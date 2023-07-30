b002322d246bfc536a5ec62d414202a86af7bd62
Mohamed <mohamed.zakri@protonmail.com>
2023-07-30 22:10:39 +0200

    added new item: sapphire added new item: raw sapphire added new creative mode tab for sapphire

--- src/main/java/it/mohamed/mmyt/TutorialMod.java
+++ src/main/java/it/mohamed/mmyt/TutorialMod.java
@@ -1,9 +1,12 @@
 package it.mohamed.mmyt;
 
 import com.mojang.logging.LogUtils;
+import it.mohamed.mmyt.item.ModCreativeModTabs;
 import it.mohamed.mmyt.item.ModItem;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,32 +19,30 @@ import org.slf4j.Logger;
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod
 {
-
     public static final String MOD_ID = "mmyt";
-
-    private static final Logger LOGGER = LogUtils.getLogger();
-
+    public static final Logger LOGGER = LogUtils.getLogger();
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
+        ModCreativeModTabs.register(modEventBus);
         ModItem.register(modEventBus);
-
         modEventBus.addListener(this::commonSetup);
-
-
         MinecraftForge.EVENT_BUS.register(this);
-
+        modEventBus.addListener(this::addCreative);
     }
-
     private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+    private void addCreative(BuildCreativeModeTabContentsEvent event){
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItem.SAPPHIRE);
+            event.accept(ModItem.RAW_SAPPHIRE);
+        }
     }
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-
         }
     }
 }
--- src/main/java/it/mohamed/mmyt/item/ModItem.java
+++ src/main/java/it/mohamed/mmyt/item/ModItem.java
@@ -8,11 +8,15 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItem {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
-    /*public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
-            ()-> new Item(new Item.Properties().g));*/
-    public static void register(IEventBus eventBus){
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
 }
